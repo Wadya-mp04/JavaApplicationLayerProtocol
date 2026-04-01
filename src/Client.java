c2e37166a32d24bf76e934e4d6f8070bb6bf363b
YahiaAbouelnasr <yhelmy0401@gmail.com>
2026-04-01 16:58:58 -0400

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,4 @@
+import java.io.FileOutputStream;
 import java.net.*;
 import java.nio.*;
 import java.nio.charset.StandardCharsets;
@@ -12,7 +13,7 @@ public class Client {
         DatagramSocket socket = new DatagramSocket();
         InetAddress address = InetAddress.getByName(serverIp);
 
-        int connectionId = 12345;
+        int connectionId = (int)(Math.random() * 1000000);
 
         byte[] filenameBytes = filename.getBytes(StandardCharsets.UTF_8);
 
@@ -35,6 +36,84 @@ public class Client {
         System.out.println("Sent request:");
         System.out.println(request);
 
-        socket.close();
+        byte[] buffer = new byte[2048];
+        DatagramPacket responseDp = new DatagramPacket(buffer, buffer.length);
+        int expectedSeq = 0;
+        boolean notDone = true;
+        FileOutputStream os = new FileOutputStream("received_" + filename);
+
+        while(notDone) {
+            socket.receive(responseDp);
+            Packet packet = Packet.fromBytes(responseDp.getData(), responseDp.getLength());
+            System.out.println("Received:");
+            System.out.println(packet);
+
+            if (packet.messageType == Protocol.ERROR){
+                System.out.println("Error:" + new String(packet.payload));
+                break;
+            }
+
+            if(packet.messageType == Protocol.DATA) {
+                if (packet.connectionId != connectionId){
+                    continue;
+                }
+                if (packet.sequenceNumber == expectedSeq) {
+                    os.write(packet.payload);
+
+                    Packet ack = new Packet(
+                        connectionId,
+                        packet.sequenceNumber,
+                        Protocol.ACK,
+                        (byte) 0,
+                        new byte[0]
+                    );
+
+                    byte[] ackBytes = ack.toBytes();
+                    DatagramPacket ackdp = new DatagramPacket(
+                        ackBytes,
+                        ackBytes.length,
+                        responseDp.getAddress(),
+                        responseDp.getPort()
+                    );
+                    socket.send(ackdp);
+
+                    expectedSeq = 1 - expectedSeq;
+
+                    if(packet.isFinal()) {
+                        notDone = false;
+                    }
+                }
+                    else {
+                        int lastexp = 1 - expectedSeq;
+                        Packet Ack = new Packet (
+                            connectionId,
+                            lastexp,
+                            Protocol.ACK,
+                            (byte) 0,
+                            new byte[0]
+                        );
+
+                        byte[] ackbytes = Ack.toBytes();
+                        DatagramPacket ackdp = new DatagramPacket(
+                            ackbytes,
+                            ackbytes.length,
+                            responseDp.getAddress(),
+                            responseDp.getPort()
+                        );
+                        socket.send(ackdp);
+                    }
+                }
+               
+            }
+            os.close();
+            socket.close();
+            System.out.println("transer complete");
+
+            
+        }
+
+    
     }
-}
+
+  
+
